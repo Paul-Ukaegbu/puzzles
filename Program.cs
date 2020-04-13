46f6d53b0130e29ccf73f7fd644322928bb7c064
paul-py07 <pauliukaegbu@gmail.com>
2020-04-13 05:18:10 -0500

    commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace puzzles
+{
+    public class Program{
+
+        public static int TossCoin(){
+
+            Random rand = new Random();
+            return rand.Next(1, 100);
+        }
+
+        static void Main(string[] args){
+
+            int coin = TossCoin();
+            if (coin % 2 == 0){
+                Console.WriteLine("Heads");
+            }
+            else{
+                Console.WriteLine("Tails");
+            }
+        }
+
+
+    }
+}
